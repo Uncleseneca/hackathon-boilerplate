6f09ce47f443188ff0338f4010facb9a8bb3a129
uncleseneca <uncleseneca@gmail.com>
2019-06-09 16:13:37 +0300

    add index page desc

--- src/features/home-page/HomePage.js
+++ src/features/home-page/HomePage.js
@@ -1,4 +1,16 @@
 import React, { useRef } from 'react'
+import s from './HomePage.module.css'
 export const HomePage = () => {
-  return <h1>Главная страница</h1>
+  return (
+    <div className={s.main}>
+      <div className={s.section}>
+        Простая, интуитивно понятная и надежная система обмена данными о
+        студентах с работодателями в обе стороны
+      </div>
+      <div className={s.section}>
+        Схема обработки информации (поиска закономерностей) для внесения
+        конструктивных изменений в образовательные процессы
+      </div>
+    </div>
+  )
 }
